93d1194a16b6f770a68c6f128cacd0b938b98c04
david michel <davidmichel77@gmail.com>
2019-05-07 19:06:57 +0200

    ok

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   handleKeyUp = (event) => {
-    console.log(event.target.value);
+    fetch(`http://api.giphy.com/v1/gifs/search&q=${event.target.value}&api_key=BuTBFM6393R9Jmhq6HQR44x3VdL9N8OB`)
+    .then(results => {
+      console.log(results);
+    });
   }
   render() {
     return (
