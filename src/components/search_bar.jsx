c767729ecc00f71f62d8c98de172994dcbea5861
david michel <davidmichel77@gmail.com>
2019-05-11 17:41:37 +0200

    save changes

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   handleKeyUp = (event) => {
-    fetch(`http://api.giphy.com/v1/gifs/search&q=${event.target.value}&api_key=BuTBFM6393R9Jmhq6HQR44x3VdL9N8OB`)
-    .then(results => {
-      console.log(results);
-    });
-  }
+    const apiKey = "BuTBFM6393R9Jmhq6HQR44x3VdL9N8OB";
+    const url = "http://api.giphy.com/v1/gifs/search&q=" + event.target.value + "&api_key=" + apiKey;
+
+    fetch(url)
+      .then((response) => {
+       return response.json();
+       console.log('ok');
+      })
+
+    };
   render() {
     return (
       <input type="text" className="form-search form-control" onChange={this.handleKeyUp} />
